4a9df7010907a6d4c237b401147bde30d8290b00
MudasirSherwani <mudasir9757@gmail.com>
2023-04-28 13:18:52 +0500

    setup devise and create devise view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+    # protect_from_forgery with: :null_session
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :set_current_user
-    before_action :authenticate_user!
-    rescue_from CanCan::AccessDenied do |exception|
-      redirect_to root_url, alert: exception.message
-    end
+    # before_action :set_current_user
+    # before_action :authenticate_user!
+    # rescue_from CanCan::AccessDenied do |exception|
+    #   redirect_to root_url, alert: exception.message
+    # end
   
     protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
     end
   
     private
