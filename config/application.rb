385f4fd77e2b3346fd5930906e4d6dc909afb4ed
MudasirSherwani <mudasir9757@gmail.com>
2023-04-28 13:23:14 +0500

    Update Gem

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module RailsBudgetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_storage.service = :local
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :users, only: [:index, :show, :new] do
     resources :categories, only: [:new, :create, :show, :index, :destroy]
     resources :transactions, only: [:new, :create, :index, :show, :destroy]
