04489b9abf0d5cb44f25b0e14a6d278ea8b4271d
MudasirSherwani <mudasir9757@gmail.com>
2023-04-29 15:34:20 +0500

    Update App controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,9 @@
 class ApplicationController < ActionController::Base
-    # protect_from_forgery with: :null_session
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    # before_action :set_current_user
-    # before_action :authenticate_user!
-    # rescue_from CanCan::AccessDenied do |exception|
-    #   redirect_to root_url, alert: exception.message
-    # end
-  
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
-    end
-  
-    private
-  
-    def set_current_user
-      @current_user = User.find_by(id: session[:user_id])
-    end
+  before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :name) }
+  end
 end
